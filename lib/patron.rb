f2907bf615fca00d7d2768a501b627b32d4eb651
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-19 09:29:51 -0700

    Test Patron can add interests

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,9 +1,10 @@
 class Patron
-  attr_reader :name, :spending_money
+  attr_reader :name, :spending_money, :interests
 
   def initialize(name, money)
     @name = name
     @spending_money = money
+    @interests = []
   end
-  
+
 end
