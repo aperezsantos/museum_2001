efdfec3d5cb01621c749e051442aeee9b6762516
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-19 09:38:28 -0700

    Create Museum test/class files, build tests for asserting instance and attributes

---
+++ test/museum_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+class MuseumTest < Minitest::Test
+
+  def test_it_exists
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+  end
+
+  def test_it_has_attributes
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+end
