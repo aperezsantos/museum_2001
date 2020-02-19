68e8d3573398722f1789c54f04ba5dd7af8c5972
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-19 09:48:56 -0700

    Add setup to Museum test file, test it starts without exhibits, include attr_reader and instance variable for exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -6,15 +6,25 @@ require './lib/exhibit'
 
 class MuseumTest < Minitest::Test
 
-  def test_it_exists
-    dmns = Museum.new("Denver Museum of Nature and Science")
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+  end
 
-    assert_instance_of Museum, dmns
+  def test_it_exists
+
+    assert_instance_of Museum, @dmns
   end
 
   def test_it_has_attributes
-    dmns = Museum.new("Denver Museum of Nature and Science")
 
-    assert_equal "Denver Museum of Nature and Science", dmns.name
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+
+  def test_it_starts_without_exhibits
+
+    assert_equal [], @dmns.exhibits
   end
 end
