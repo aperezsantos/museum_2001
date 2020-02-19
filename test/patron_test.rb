dd82a9f44c4ec03a296f5aee951dfcfc8aab7190
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-19 09:21:02 -0700

    Initialize patron with parameters, fix naming issue with instance, pass initial test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -8,6 +8,6 @@ class PatronTest < Minitest::Test
   def test_it_exists
     patron_1 = Patron.new("Bob", 20)
 
-    assert_instance_of Patron, patron
+    assert_instance_of Patron, patron_1
   end
 end
